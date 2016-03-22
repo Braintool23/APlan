9d7764bf4e95396f04b4fa85a8c93149478257d6
Bernhard Reichl <b.reichl@braintool.com>
2016-03-22 14:22:53 +0000

    Changes from Views service

--- Test/components/converter/index.js
+++ Test/components/converter/index.js
@@ -7,19 +7,18 @@ app.converter = kendo.observable({
 
 // START_CUSTOM_CODE_converter
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-	
-	var model = app.converter.converterModel;
-	model.set("submit", function() {
+
+var model = app.converter.converterModel;
+model.set("submit", function() {
     var lbs = model.get("fields.kg") * 2.2046; // get fields.kgs and multiply with 2.2046
     model.set("fields.pound", lbs + " lbs"); // set fields.result
-	});
+});
 
 // END_CUSTOM_CODE_converter
 (function(parent) {
     var converterModel = kendo.observable({
         fields: {
             pound: '',
-            group: '',
             kg: '',
         },
         submit: function() {},
