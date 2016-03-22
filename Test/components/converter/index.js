bbebe2bcf1cefd76df74b04d47990d1283f012a3
Bernhard Reichl <b.reichl@braintool.com>
2016-03-22 19:52:56 +0000

    Changes from Views service

--- Test/components/converter/index.js
+++ Test/components/converter/index.js
@@ -10,19 +10,39 @@ app.converter = kendo.observable({
 
 var model = app.converter.converterModel;
 model.set("submit", function() {
-    var lbs = model.get("fields.kg") * 2.2046; // get fields.kgs and multiply with 2.2046
-    model.set("fields.pound", lbs + " lbs"); // set fields.result
+    var lbs = model.get("fields.kgs") * 2.2046; // get fields.kgs and multiply with 2.2046
+    model.set("fields.result", lbs + " lbs"); // set fields.result
 });
+
+/*(function (global) {
+    var LoginViewModel,
+        app = global.app = global.app || {};
+
+    LoginViewModel = kendo.data.ObservableObject.extend({
+        kgs: '',
+        result: '',
+
+        onLogin: function () {
+            var that = this;
+               // kgs = that.get("kgs"),
+                that.set("result", 10);
+         },
+
+    });
+
+    app.loginService = {
+        viewModel: new LoginViewModel()
+    };
+})(window); */
 
 // END_CUSTOM_CODE_converter
 (function(parent) {
     var converterModel = kendo.observable({
         fields: {
-            pound: '',
-            kg: '',
+            result: '',
+            kgs: '',
         },
-        submit: function() {},
-        cancel: function() {}
+        submit: function() {}
     });
 
     parent.set('converterModel', converterModel);
